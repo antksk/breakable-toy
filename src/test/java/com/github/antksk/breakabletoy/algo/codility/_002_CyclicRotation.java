10230eb57ae615db9e900f419f36ec8bbe83d5cb
newton <newton@wemakeprice.com>
2018-07-18 12:32:01 +0900

    codility 시작

---
+++ src/test/java/com/github/antksk/breakabletoy/algo/codility/_002_CyclicRotation.java
@@ -0,0 +1,27 @@
+package com.github.antksk.breakabletoy.algo.codility;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
+public class _002_CyclicRotation {
+    class Solution {
+        public int[] solution(int[] A, int K){
+            final int aLength = A.length;
+            final int[] result = new int[aLength];
+
+            for (int i = 0; i < aLength; i++) {
+                result[(i+K) % aLength] = A[i];
+            }
+            return result;
+        }
+    }
+
+    @Test
+    public void test(){
+        Solution s = new Solution();
+        log.debug("{}", s.solution(new int[]{3, 8, 9, 7, 6}, 3));
+        log.debug("{}", s.solution(new int[]{0, 0, 0}, 1));
+        log.debug("{}", s.solution(new int[]{1, 2, 3, 4}, 4));
+    }
+}
