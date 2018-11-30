0d8ac945b99d6ac698f4ac1db99d275d9ce5e092
a1101049 <seunggyum.kim@sk.com>
2018-11-30 09:38:05 +0900

    url 테스트 - 동명의 underscore 스타일과 lower camel 스타일에 대해 같은 값 받기 테스트 - 같은 url에 대해서 다른 파라미터를 받을 때, 각각 다른 메소드 동작 확인 테스트

--- src/main/java/com/github/antksk/breakabletoy/spring/GetMappingTestController.java
+++ src/main/java/com/github/antksk/breakabletoy/spring/GetMappingTestController.java
@@ -1,28 +1,41 @@
 package com.github.antksk.breakabletoy.spring;
 
+import com.google.common.base.CaseFormat;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
 @Slf4j
 @RestController
+@RequestMapping(path = "/test")
 public class GetMappingTestController {
 
-//    @GetMapping("/test")
-//    public String test(){
-//        return "test()";
-//    }
 
-    @GetMapping(path = "/test")
-    public String test(){
-        String result = "test()";
-        log.debug("result : {}, param : {}", result, "");
+    public String getSmartParam(HttpServletRequest request, String paramName, String defaultValue ){
+        final String underscoreToCamelParam = CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, paramName);
+        return Optional.ofNullable(request.getParameter(paramName))
+                .orElse(Optional.ofNullable(request.getParameter(underscoreToCamelParam))
+                        .orElse(defaultValue));
+    }
+
+    @GetMapping("/param")
+    public String test(HttpServletRequest request){
+        String result = "test(HttpServletRequest request)";
+
+        final String paramName = CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, "user_name");
+        // CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_HYPHEN, "userName")
+
+        System.out.println("param name : " + paramName + ", value : " + getSmartParam(request, "user_name", "no param"));
         return result;
 
     }
 
-    @GetMapping(path = "/test", params = {"a"})
+    @GetMapping(params = {"a"})
     public String test(@RequestParam(required = false) String a){
         String result = "test(@RequestParam String a)";
         log.debug("result : {}, param : {}", result, a);
@@ -30,7 +43,7 @@ public class GetMappingTestController {
 
     }
 
-    @GetMapping(path="/test",params = {"b"})
+    @GetMapping(params = {"b"})
     public String test(@RequestParam(required = false) Integer b){
         String result = "test(@RequestParam Integer b)";
         log.debug("result : {}, param : {}", result, b);
--- src/test/java/com/github/antksk/breakabletoy/spring/GetMappingTestControllerTest.java
+++ src/test/java/com/github/antksk/breakabletoy/spring/GetMappingTestControllerTest.java
@@ -1,5 +1,7 @@
 package com.github.antksk.breakabletoy.spring;
 
+import com.google.common.base.CaseFormat;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Slf4j
 @RunWith(SpringRunner.class)
 @WebMvcTest(GetMappingTestController.class)
 public class GetMappingTestControllerTest {
@@ -20,20 +23,40 @@ public class GetMappingTestControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    /**
+     * 동명의 underscore 스타일과 lowerCamel 스타일의 파라미터가 컨트롤러에 들어왔을때,
+     * 정상적으로 값을 가져 올수 있도록 코드 작성
+     */
     @Test
-    public void test_url_param_a() throws Exception {
+    public void underscore_to_lower_camel_test() throws Exception{
+        System.out.println("user_name : " + CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, "user_name"));
+        System.out.println("userName : " + CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_CAMEL, "userName"));
+
+        mvc.perform(get("/test/param").accept(MediaType.TEXT_PLAIN).param("user_name","underscore param"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("test(HttpServletRequest request)"))
+        ;
+        mvc.perform(get("/test/param").accept(MediaType.TEXT_PLAIN).param("userName","camel param"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("test(HttpServletRequest request)"))
+        ;
+    }
+
+
+    /**
+     * 같은 url로 다른 이름의 파라미터가 들어왔을때, 각 각 다른 메소드가 동작하는지 테스트
+     */
+    @Test
+    public void same_param_test() throws Exception {
         mvc.perform(get("/test").accept(MediaType.TEXT_PLAIN).param("a","test code"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("test(@RequestParam String a)"))
         ;
-    }
-
-    @Test
-    public void test_url_param_b() throws Exception {
         mvc.perform(get("/test").accept(MediaType.TEXT_PLAIN).param("b","1234"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("test(@RequestParam Integer b)"))
         ;
     }
+
 
 }
